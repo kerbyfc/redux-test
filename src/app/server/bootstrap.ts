37c5a488821b7ec93f4dbc101dda424d196c670b
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-01 16:10:04 +0300

    send index.html by server

--- src/app/server/bootstrap.ts
+++ src/app/server/bootstrap.ts
@@ -1,18 +1,23 @@
+import * as _ from 'lodash';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as express from 'express';
 
 const port = 3001;
 const app = express();
+const layout: string = fs.readFileSync(path.join(__dirname, '../index.html')).toString();
 
 if (process.env.NODE_ENV !== 'production') {
-    require('./../../webpack.dev.js')(app);
+    require('./../../../webpack.dev.js')(app);
 }
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use((req, res) => {
-    res.send('Hello app!');
+    const html = 'Initial';
+    const state = {};
+
+    res.end(_.template(layout)({html, state}));
 });
 
 app.listen(port, function() {
