807d0fa2b4724adc13d552f6be420776f87e2df4
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-19 00:13:54 +0300

    remove extra typings

--- src/containers/Document/Document.tsx
+++ src/containers/Document/Document.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import "react-dom";
-import { renderToString } from "react-dom/server";
-import * as Helmet from "react-helmet";
-import serialize = require("serialize-javascript");
+import * as React from 'react';
+import 'react-dom';
+import { renderToString } from 'react-dom/server';
+import * as Helmet from 'react-helmet';
+import serialize = require('serialize-javascript');
 
 interface IDocumentProps extends React.ClassAttributes<Document> {
   app?: JSX.Element;
@@ -28,34 +28,34 @@ export default class Document extends React.Component<IDocumentProps, {}> {
    * @return {JSX.Element} Rendered document.
    */
   public render() {
-    const app = this.props.app ? renderToString(this.props.app) : "";
+    const app = this.props.app ? renderToString(this.props.app) : '';
     const assets = this.props.assets || {};
     const state = this.props.store.getState();
     const head = this.props.server ? Helmet.rewind() : null;
-    const fonts = "https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic";
+    const fonts = 'https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic';
 
     return (
       <html>
         <head>
           {head && head.title.toComponent()}
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='shortcut icon' href='/favicon.ico' />
           {assets && Object.keys(assets).filter(chunk => !!assets[chunk].css).map(chunk => (
-            <link key={`asset.${chunk}.css`} href={assets[chunk].css} rel="stylesheet" type="text/css" />
+            <link key={`asset.${chunk}.css`} href={assets[chunk].css} rel='stylesheet' type='text/css' />
           ))}
-          <link href={fonts} rel="stylesheet" type="text/css" />
+          <link href={fonts} rel='stylesheet' type='text/css' />
         </head>
         <body>
           <div
-            className="app"
-            id="root"
+            className='app'
+            id='root'
             dangerouslySetInnerHTML={{__html: app}}
           />
-          <script id="preloaded" type="text/javascript" dangerouslySetInnerHTML={{
+          <script id='preloaded' type='text/javascript' dangerouslySetInnerHTML={{
             __html: `window.__PRELOADED__=${serialize(state)};`,
           }} />
           {assets && Object.keys(assets).filter(chunk => !!assets[chunk].js).map(chunk => (
-            <script key={`asset.${chunk}.js`} src={assets[chunk].js} type="text/javascript" />
+            <script key={`asset.${chunk}.js`} src={assets[chunk].js} type='text/javascript' />
           ))}
         </body>
       </html>
