4d20ec0c28c63d40e0c52d8f62459127580b2265
Alexander Lukin <alexander.lukin@acronis.com>
2016-10-13 20:18:42 +0300

    fix(hooks): fix hooks

--- scripts/initializers/githooks.ts
+++ scripts/initializers/githooks.ts
@@ -1,4 +1,4 @@
 import {f} from '../utils';
 
-f('.git/hooks/pre-commit').write(f('scripts/wizards/commit.ts').es5, 777).shebang();
-f('.git/hooks/prepare-commit-msg').write(f('scripts/validators/tabs.ts').es5, 777).shebang();
+f('.git/hooks/prepare-commit-msg').write(f('scripts/wizards/commit.ts').es5, 777).shebang();
+f('.git/hooks/pre-commit').write(f('scripts/validators/tabs.ts').es5, 777).shebang();
